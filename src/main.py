63ce1a5c9539038edb78a86988aa8c634ea9174c
Gregorh98 <33038097+Gregorh98@users.noreply.github.com>
2026-06-05 11:57:51 +0100

    3 ring burst

--- src/main.py
+++ src/main.py
@@ -9,4 +9,4 @@ DIAL_PULSE_PIN = 27
 if __name__ == "__main__":
     print("Running")
     phone = Phone(HOOK_SWITCH_PIN, RINGER_COIL_POS_PIN, RINGER_COIL_NEG_PIN, DIAL_ENABLE_PIN, DIAL_PULSE_PIN)
-    phone.ring()
+    phone.ring(3)
